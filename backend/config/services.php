14a3b264e689aa67a74dd2452584eb29d9511e40
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-30 14:31:35 +0530

    Configure C-Net Store central sync

--- backend/config/services.php
+++ backend/config/services.php
@@ -6,5 +6,12 @@ return [
         'key_secret' => env('RAZORPAY_KEY_SECRET'),
         'webhook_secret' => env('RAZORPAY_WEBHOOK_SECRET'),
     ],
-];
 
+    'mci_central' => [
+        'enabled' => filter_var(env('MCI_CENTRAL_SYNC_ENABLED', false), FILTER_VALIDATE_BOOL),
+        'url' => rtrim((string) env('MCI_CENTRAL_URL', 'https://mciedu.in'), '/'),
+        'token' => env('MCI_CENTRAL_TOKEN'),
+        'business_code' => env('MCI_CENTRAL_BUSINESS_CODE', 'c-net-store'),
+        'timeout' => (int) env('MCI_CENTRAL_TIMEOUT', 10),
+    ],
+];
