700c889a2745188732479446dea6597b65945a68
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-09-04 11:35:24 +0530

    Include legal pages in standard database seeding

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void { $this->call(CoreRoleSeeder::class); }
+    public function run(): void
+    {
+        $this->call([
+            CoreRoleSeeder::class,
+            LegalContentSeeder::class,
+        ]);
+    }
 }
